56ca7dfb1adefa723873ab4e9079334aa5784830
Dave Rosoff <drosoff@collegeofidaho.edu>
2020-12-03 00:32:45 -0700

    Tail recursion

--- 2020/01/01.py
+++ 2020/01/01.py
@@ -31,30 +31,31 @@ elapsed = t1 - t0
 print(f"Part Two: {p2:d}, {1e6 * elapsed:0.0f} µsec")
 
 # recursive solution
+# tail recursion uses accumulator to keep running product
 
-def prob_one(report_l, n, target):
+def prob_one(report_l, n, target, acc):
   if n == 0 or len(report_l) == 0:
     return None # not found
 
   if n == 1:
     if target in report_l:
-      return target
+      return target * acc
 
   for i in range(len(report_l)):
     subprob = [v for (j, v) in enumerate(report_l) if j != i]
-    subsoln = prob_one(subprob, n-1, target - report_l[i])
+    subsoln = prob_one(subprob, n-1, target - report_l[i], acc * report_l[i])
     if subsoln:
-      return report_l[i] * subsoln
+      return subsoln
 
 t0 = time.perf_counter()
-p1 = prob_one(reports, 2, 2020)
+p1 = prob_one(reports, 2, 2020, 1)
 t1 = time.perf_counter()
 elapsed = t1 - t0
 
 print(f"Part One: {p1:d}, {1e6*elapsed:0.0f} µsec")
 
 t0 = time.perf_counter()
-p1 = prob_one(reports, 3, 2020)
+p1 = prob_one(reports, 3, 2020, 1)
 t1 = time.perf_counter()
 elapsed = t1 - t0
 
