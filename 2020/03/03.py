9586120d6894d8f924ba422b625f5916dcaf8d40
Dave Rosoff <drosoff@collegeofidaho.edu>
2020-12-03 09:11:42 -0700

    Identifiers

--- 2020/03/03.py
+++ 2020/03/03.py
@@ -3,7 +3,7 @@ import time
 with open("input", 'r') as f:
   ourmap = [line[:-1] for line in f.readlines()]
 
-def count_trees(ourmap, deltax, deltay):
+def count_trees(ourmap, dx, dy):
   result = 0
   x = 0
   y = 0
@@ -11,22 +11,22 @@ def count_trees(ourmap, deltax, deltay):
   while y < len(ourmap):
     if ourmap[y][x] == '#':
       result += 1
-    x = (x + deltax) % len(ourmap[0])
-    y += deltay
+    x = (x + dx) % len(ourmap[0])
+    y += dy
 
   return result
 
 tic = time.perf_counter()
-p0 = count_trees(ourmap, 3, 1)
+p1 = count_trees(ourmap, 3, 1)
 toc = time.perf_counter()
-print(f"Part One: {p0:d}, {1e6*(toc-tic):0.0f} µsec")
+print(f"Part One: {p1:d}, {1e6*(toc-tic):0.0f} µsec")
 
-part_two = 1
+p2 = 1
 tic = time.perf_counter()
-part_two *= count_trees(ourmap, 1,1)
-part_two *= count_trees(ourmap, 3,1)
-part_two *= count_trees(ourmap, 5,1)
-part_two *= count_trees(ourmap, 7,1)
-part_two *= count_trees(ourmap, 1,2)
+p2 *= count_trees(ourmap, 1,1)
+p2 *= count_trees(ourmap, 3,1)
+p2 *= count_trees(ourmap, 5,1)
+p2 *= count_trees(ourmap, 7,1)
+p2 *= count_trees(ourmap, 1,2)
 toc = time.perf_counter()
-print(f"Part Two: {part_two:d}, {1e6*(toc-tic):0.0f} µsec")
+print(f"Part Two: {p2:d}, {1e6*(toc-tic):0.0f} µsec")
