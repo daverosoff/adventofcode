e5320d238905a51e87e08edda2ca655a9199e53b
Dave Rosoff <drosoff@collegeofidaho.edu>
2022-10-08 21:00:10 -0600

    Rust stuff

---
+++ 2020/01/rust01/target/debug/deps/rust01-61bb1ef7117d5836.d
@@ -0,0 +1,5 @@
+/home/dave/adventofcode/2020/01/rust01/target/debug/deps/rust01-61bb1ef7117d5836: src/main.rs
+
+/home/dave/adventofcode/2020/01/rust01/target/debug/deps/rust01-61bb1ef7117d5836.d: src/main.rs
+
+src/main.rs:
---
+++ 2020/01/rust01/target/debug/rust01.d
@@ -0,0 +1,1 @@
+/home/dave/adventofcode/2020/01/rust01/target/debug/rust01: /home/dave/adventofcode/2020/01/rust01/src/main.rs
