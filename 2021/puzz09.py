2e4547ac65c0580955e8a297ec23d0e2b5bf94ad
Dave Rosoff <drosoff@collegeofidaho.edu>
2022-10-08 20:57:05 -0600

    Catchup

--- 2021/puzz09.py
+++ 2021/puzz09.py
@@ -1,34 +1,57 @@
+from math import prod
+
 sample = """2199943210
 3987894921
 9856789892
 8767896789
 9899965678"""
 
-def puzzle(data, part):
-    arr = [[int(ch) for ch in row.strip()] for row in data.split('\n') if row]
+def get_naybs(arr, x, y):
     xmin = 0
     ymin = 0
     xmax = len(arr[0]) - 1
     ymax = len(arr) - 1
-    def pos_is_min(x, y):
-        poss = [(x - 1, y), (x, y - 1), (x + 1, y), (x, y + 1)]
-        check = [(i, j) for (i, j) in poss if i >= xmin and i <= xmax and j >= ymin and j <= ymax]
-        return all([arr[y][x] < arr[j][i] for (i, j) in check])
-    mins = []
-    if part == 1:
-        result = 0
-        for x in range(xmax + 1):
-            for y in range(ymax + 1):
-                if pos_is_min(x, y):
-                    result += (1 + arr[y][x])
-                    mins.append((x, y))
-    else:
-        result = 1
-        
+    poss = [(x - 1, y), (x, y - 1), (x + 1, y), (x, y + 1)]
+    naybs = set([(i, j) for (i, j) in poss if (xmin <= i <= xmax) and (ymin <= j <= ymax)])
+    return naybs
 
-    return result
+def is_min(arr, x, y):
+    naybs = get_naybs(arr, x, y)
+    return all([arr[y][x] < arr[j][i] for (i, j) in naybs])
+
+def get_basin_size(arr, x, y):
+    assert(arr[y][x] < 9)
+    basin = set([(x, y)])
+    naybs = get_naybs(arr, x, y)
+    while not all([(x, y) in basin for (x, y) in naybs if arr[y][x] < 9]):
+        for (x, y) in naybs:
+            if arr[y][x] < 9:
+                basin.add((x, y))
+            # naybs = naybs.union(get_naybs(arr, x, y))
+            for (i, j) in get_naybs(arr, x, y):
+                if arr[y][x] < 9:
+                    naybs.add((x, y))
+    return len(basin)
+
+def puzzle(data):
+    arr = [[int(ch) for ch in row.strip()] for row in data.split('\n') if row]
+    xmax = len(arr[0]) - 1
+    ymax = len(arr) - 1
+
+    mins = []
+    part_one = 0
+    for x in range(xmax + 1):
+        for y in range(ymax + 1):
+            if is_min(arr, x, y):
+                part_one += (1 + arr[y][x])
+                mins.append((x, y))
+    basin_sizes = sorted([get_basin_size(arr, x, y) for (x, y) in mins], reverse=True)
+    part_two = prod(basin_sizes[:3])
+    return part_one, part_two
+
+
 
 with open('input09.dat') as file:
     input = file.read()
 
-print(puzzle(input))
+print(puzzle(sample))
