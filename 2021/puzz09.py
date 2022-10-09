8c5de91ffb1701c89f0421c9a41b264d53629c25
Dave Rosoff <drosoff@collegeofidaho.edu>
2022-10-08 22:10:07 -0600

    Finished

--- 2021/puzz09.py
+++ 2021/puzz09.py
@@ -1,3 +1,6 @@
+import random
+from collections import deque
+
 sample = """2199943210
 3987894921
 9856789892
@@ -11,26 +14,50 @@ def puzzle(data, part):
     ymin = 0
     xmax = len(arr[0]) - 1
     ymax = len(arr) - 1
-    def pos_is_min(x, y):
+    def is_valid(i, j):
+        return xmin <= i <= xmax and ymin <= j <= ymax
+    def naybs(x, y):
         poss = [(x - 1, y), (x, y - 1), (x + 1, y), (x, y + 1)]
-        check = [(i, j) for (i, j) in poss if xmin <= i <= xmax and ymin <= j <= ymax]
-        return all([arr[y][x] < arr[j][i] for (i, j) in check])
-    mins = []
-    # if part == 1:
-    part_one_result = 0
-    for x in range(xmax + 1):
-        for y in range(ymax + 1):
-            if pos_is_min(x, y):
-                part_one_result += (1 + arr[y][x])
-                mins.append((x, y))
-    # else:
-    part_two_result = 1
-    arr2 = [[9 if x == 9 else 0 for x in row] for row in arr]
+        check = [(i, j) for (i, j) in poss if is_valid(i, j)]
+        return check
+    def pos_is_min(x, y):
+        return all([arr[y][x] < arr[j][i] for (i, j) in naybs(x, y)])
+    if part == 1:
+        part_one_result = 0
+        mins = []
+        for x in range(xmax + 1):
+            for y in range(ymax + 1):
+                if pos_is_min(x, y):
+                    part_one_result += (1 + arr[y][x])
+                    mins.append((x, y))
+        return part_one_result
+    else:
+        lookup_table = [(x, y) for x in range(xmax + 1)
+                               for y in range(ymax + 1) if arr[y][x] < 9]
+        def find_basin():
+            x, y = lookup_table[0]
+            count = 0
+            def find_basin_helper(x, y):
+                nonlocal count
+                lookup_table.remove((x, y))
+                # print(f"removing ({x}, {y})")
+                count += 1
+                # print(f"current basin is size {count}")
+                for n in naybs(x, y):
+                    if n in lookup_table:
+                        find_basin_helper(*n)
+                return count
+            return find_basin_helper(x, y)
+        basins = []
+        while lookup_table:
+            basins.append(find_basin())
+        part_two_result = 1
+        for b in sorted(basins, reverse=True)[:3]:
+            part_two_result *= b
+        return part_two_result
 
-
-    return result
-
-with open('input09.dat') as file:
+with open('2021/input09.dat') as file:
     input = file.read()
 
-print(puzzle(input))
+print(puzzle(input, 1))
+print(puzzle(input, 2))
