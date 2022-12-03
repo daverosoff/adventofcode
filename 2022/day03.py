22e714d7391d32648b25f19793b3425c00a232a1
Dave Rosoff <drosoff@collegeofidaho.edu>
2022-12-02 23:35:12 -0700

    Solved

---
+++ 2022/day03.py
@@ -0,0 +1,45 @@
+import string
+
+sample = """vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw"""
+
+def get_priority(item: str) -> int:
+    if item in string.ascii_lowercase:
+        return ord(item) - ord('a') + 1
+    else:
+        return ord(item) - ord('A') + 27
+
+def left_half(items: str) -> str:
+    ell = len(items)
+    return items[ :ell//2]
+
+def right_half(items: str) -> str:
+    ell = len(items)
+    return items[ell//2: ]
+
+def find_match(items: str) -> str:
+    left, right = left_half(items), right_half(items)
+    match = [ch for ch in left if ch in right][0]
+    return match
+
+def part_one(inp: str) -> int:
+    return sum([get_priority(find_match(line)) for line in inp.splitlines()])
+
+def part_two(inp: str) -> int:
+    result = 0
+    for i in range(0, len(input := inp.splitlines()), 3):
+        group = input[i:i+3]
+        a, b, c = [set(elf) for elf in group]
+        (match,) = a.intersection(b).intersection(c)
+        result += get_priority(match)
+    return result
+
+with open("input03.dat") as f:
+    puzzle = f.read()
+
+print(f"part one: {part_one(puzzle)}")
+print(f"part two: {part_two(puzzle)}")
